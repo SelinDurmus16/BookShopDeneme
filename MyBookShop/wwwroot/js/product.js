7256106e5141befd929fbaccec0f913ea821289c
SelinDurmus16 <durmusselin8@gmail.com>
2024-08-14 11:52:01 +0300

    edit button is newly written for API

--- MyBookShop/wwwroot/js/product.js
+++ MyBookShop/wwwroot/js/product.js
@@ -1,7 +1,6 @@
 ﻿$(document).ready(function () {
     loadDataTable();
 });
-
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
@@ -9,8 +8,18 @@ function loadDataTable() {
             { data: 'title', "width": "25%" },
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
-            { data: 'author', "width": "20%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
